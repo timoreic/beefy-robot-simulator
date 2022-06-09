690d0acb4da990f0debb5d2e9920f9f45fd05dda
Timo <t.reichelt@live.de>
2022-06-09 17:11:49 +0800

    Implemented report method including tests

--- out/BeefyRobot.js
+++ out/BeefyRobot.js
@@ -8,8 +8,18 @@ class BeefyRobot {
         const x = parseInt(position[6]);
         const y = parseInt(position[8]);
         if (x >= 0 && x <= 4 && y >= 0 && y <= 4) {
-            this.position = position.substring(5).split(',');
+            this.position = position.substring(6).split(',');
         }
+    }
+    report() {
+        let report;
+        if (this.position) {
+            report = this.position.join(',');
+        }
+        else {
+            report = 'BeefyRobot is not on the table';
+        }
+        return report;
     }
 }
 exports.default = BeefyRobot;
